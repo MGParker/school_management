adeb27a301d873a1032d189be1a56c39859b50f7
DominicPrzygonski <domprzygonski@gmail.com>
2022-06-18 19:12:39 +0200

    Q5 Attempt

--- src/main/java/za/ac/cput/service/IEmployeeService.java
+++ src/main/java/za/ac/cput/service/IEmployeeService.java
@@ -7,9 +7,16 @@ package za.ac.cput.service;
 
 import za.ac.cput.domain.Address;
 import za.ac.cput.domain.Employee;
+import za.ac.cput.domain.Name;
 
 import java.util.List;
 
 public interface IEmployeeService extends IService<Employee, String>{
     List<Employee> getAll();
+
+    //Question 5
+    Name getEmployeeNameByEmail(String email);
+
+    //Question 6
+    List<Name> getEmployeeNamesByCity(String cityID);
 }
